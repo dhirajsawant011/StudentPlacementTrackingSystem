4133a93a3292f78aecf9d208a69217f8f52403a0
dhruv-yelmame <dhruvyelmame6@gmail.com>
2026-05-11 12:58:58 +0530

    a

--- StudentPlacementTrackingSystem/src/main/java/com/spts/controler/ServletRegister.java
+++ StudentPlacementTrackingSystem/src/main/java/com/spts/controler/ServletRegister.java
@@ -8,33 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-/**
- * Servlet implementation class ServletRegister
- */
 @WebServlet("/ServletRegister")
 public class ServletRegister extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletRegister() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html")	;
-		PrintWriter out = response.getWriter();
 		
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
