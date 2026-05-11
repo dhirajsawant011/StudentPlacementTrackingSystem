546d3847f98623c705d1fdc7e0c241ef60c20360
dhruv-yelmame <dhruvyelmame6@gmail.com>
2026-05-11 15:22:50 +0530

    m

---
+++ StudentPlacementTrackingSystem/src/main/java/com/spts/controler/ServletLogin.java
@@ -0,0 +1,59 @@
+package com.spts.controler;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet("/validate")
+public class ServletLogin extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet", "root", "8010865586");
+			
+			PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM user WHERE username=? AND password=?");
+			pstmt.setString(1, username);
+            pstmt.setString(2, password);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+
+        		RequestDispatcher r = request.getRequestDispatcher("dashboard.html");
+        		r.forward(request, response);
+        	
+            		out.println("<h2>Login Successful</h2>");
+                
+            } else {
+                out.println("<h2>Invalid Username or Password</h2>");
+            }
+			
+		} catch (Exception e) {
+			
+			out.println(e);
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
